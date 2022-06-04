f698cb06b0fa55d3148d20bd0c3ee22b1efdc6c8
jsfix.live analysis <info@coana.tech>
2022-06-04 13:34:35 +0200

    JSFIX patches

--- src/commands/bootstrap.js
+++ src/commands/bootstrap.js
@@ -11,7 +11,10 @@ class BootstrapCommand extends Command {
     const dest = join(process.cwd(), 'src', 'core');
 
     await ensureDir(dest);
-    await copySync(src, dest);
+    await /* TODO: JSFIX could not patch the breaking change:
+    Allow copying broken symlinks 
+    Suggested fix: You can use the exists and existsSync functions https://nodejs.org/api/fs.html#fsexistspath-callback from the fs module to check if a symlink is broken. */
+    copySync(src, dest);
 
     console.log(chalk.green('CREATE '), `src/core/application/commands/index.ts`);
     console.log(chalk.green('CREATE '), `src/core/application/index.ts`);
